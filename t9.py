49d33cb91bf09be8c13c83b8f4ab6b1306233f67
Danilo de Jesus da Silva Bellini <danilo.bellini@gmail.com>
2013-08-24 10:13:31 -0300

    first commit

---
+++ t9.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 24 10:09:27 2013
+
+@author: danilo
+"""
+
+def t9(msg):
+  return "2"
+  
---
+++ t9_test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 24 10:10:07 2013
+
+@author: danilo
+"""
+
+from t9 import t9
+
+def test_a():
+  assert t9("a") == "2"
