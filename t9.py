fc651f8d595f6ac1b02f830148dd9146f817537b
Danilo de Jesus da Silva Bellini <danilo.bellini@gmail.com>
2013-08-24 10:21:52 -0300

    two tests mapping directly

--- t9.py
+++ t9.py
@@ -6,5 +6,18 @@ Created on Sat Aug 24 10:09:27 2013
 """
 
 def t9(msg):
-  return "2"
+  assert isinstance(msg, str)
+  data = {
+    "a": "2",
+    "b": "22",
+    "d": "3",
+    "e": "33",
+    "g": "4",
+    "h": "44",
+    "j": "5",
+    "k": "55",
+    "m": "6",
+    "n": "66",
+  }
+  return data[msg]
   
--- t9_test.py
+++ t9_test.py
@@ -7,5 +7,16 @@ Created on Sat Aug 24 10:10:07 2013
 
 from t9 import t9
 
-def test_a():
+def test_one_letter():
   assert t9("a") == "2"
+  assert t9("d") == "3"
+  assert t9("g") == "4"
+  assert t9("j") == "5"
+  assert t9("m") == "6"
+
+def test_two_letters():
+  assert t9("b") == "22"
+  assert t9("e") == "33"
+  assert t9("h") == "44"
+  assert t9("k") == "55"
+  assert t9("n") == "66"
